f77762f3045b082619908315f5b8950439999a1a
vorkna <vorkna@proton.me>
2025-01-17 14:05:36 +0700

    feat: wolf rabbit grass

--- main.py
+++ main.py
@@ -2,82 +2,154 @@ import random
 
 class Field:
     def __init__(self,wolfrange,rabbitrange,grassrange):
-        self.wolfs = [Wolf() for i in wolf]
-        self.rabbits = [Rabbit() for i in rabbit]
-        self.grasses = [Grass() for i in grass]
+        self.wolfs = [Wolf() for i in range(wolfrange)]
+        self.rabbits = [Rabbit() for i in range(rabbitrange)]
+        self.grasses = [Grass() for i in range(grassrange)]
         self.wolf = wolfrange
         self.rabbit = rabbitrange
         self.grass = grassrange
 
     def step(self):
-        for w,r,g in zip(self.wolfs, self.rabbits,self.grasses):
+        self.wolf = len(self.wolfs)
+        self.rabbit = len(self.rabbits)
+        self.grass = len(self.grasses)
+        for w in self.wolfs:
             w.live()
+            self.update(w)
+            self.consume(w)
+            self.produce(w)
+        for r in self.rabbits:
             r.live()
-            g.live()
+            self.update(r)
+            self.consume(r)
+            self.produce(r)
 
-            if w.dead:
-                self.wolf -= 1
-            if r.dead:
-                self.rabbit -= 1
-            if g.dead:
-                self.grass -= 1
+        if len(self.grasses) <= 0 :
+            self.grasses.append(Grass())
+        self.produce(self.grasses[0])
+
+    def update(self,X):
+        if not X.dead:
+            return
+        match X:
+            case Rabbit():
+                self.rabbits.remove(X)
+            case Wolf():
+                self.wolfs.remove(X)
+
+
+    def consume(self,X):
+        if isinstance(X,Grass):
+            return
+        if random.randrange(2) > 0:
+            return
+        if X.food < X.maxfood:
+            match X:
+                case Rabbit():
+                    if len(self.grasses) > 0:
+                        X.food += X.nutrient
+                        X.lifespan = X.maxlifespan
+                        self.grasses.pop()
+                case Wolf():
+                    if len(self.rabbits) > 0:
+                        X.food += X.nutrient
+                        X.lifespan = X.maxlifespan
+                        self.rabbits.pop()
+
+    def produce(self,X):
+        if X is None:
+            return
+        if isinstance(X,Grass):
+            for i in range(X.growth):
+                self.grasses.append(Grass())
+            return
+        # if random.randrange(2) > 0:
+        #     return
+        if X.food >= X.reproducefood and X.age >= X.reproduceage:
+            match X:
+                case Rabbit():
+                    self.rabbits.append(Rabbit())
+                case Wolf():
+                    self.wolfs.append(Wolf())
+
 
 class Grass(Field):
     def __init__(self):
         self.max = 400
         self.growth = 5
-        self.nutrient = 10
         self.reproduceage = 10
-
-    def live(self):
-        offspring()
-
-    def offspring(self):
-        super().grasses.push(Grass())
+        self.dead = False
 
 class Rabbit(Field):
     def __init__(self):
         self.nutrient = 10
+        self.food = 0
         self.maxfood = 45
         self.metabo = 3
-        self.reproduce = 10
+        self.reproduceage = 10
+        self.reproducefood = 40
+        self.age = 0
+        self.maxage = 25
+        self.lifespan = 3
+        self.maxlifespan = 3
+        self.dead = False
 
     def live(self):
-        self.lifespan -= 1
-        if random.randrange(2) > 0:
-            self.eat()
-        populate()
-        die()
+        if self.dead:
+            return
 
-    def eat(self):
-        if self.food < self.maxfood:
-            self.food += 10
-            self.lifespan = 2
-            super().grasses.push(Grass())
+        self.age +=1
+        self.food -= self.metabo
+        if self.food <=0:
+            self.food = 0
+            self.lifespan -= 1
+        self.die()
+
 
     def die(self):
-        if self.food and self.lifespan == 0:
+        if self.food and self.lifespan == 0 or self.age >= self.maxage:
             self.dead = True
 
-    def offspring(self):
-        if self.food > 40:
-            self.food == 0
-            if random.randrange(2) > 0:
-                super().rabbits.push(Rabbit())
 
 class Wolf(Field):
     def __init__(self):
+        self.nutrient = 10
+        self.food = 0
         self.maxfood = 200
+        self.metabo = 2
+        self.reproduceage = 10
+        self.reproducefood = 120
+        self.age = 0
+        self.maxage = 50
+        self.lifespan = 2
+        self.maxlifespan = 2
+        self.dead = False
 
+    def live(self):
+        if self.dead:
+            return
+
+        self.age +=1
+        self.food -= self.metabo
+        if self.food <=0:
+            self.food = 0
+            self.lifespan -= 1
+        self.die()
+
+    def die(self):
+        if self.food and self.lifespan == 0 or self.age >= self.maxage:
+            self.dead = True
 
 def main():
     start = 0
-    field = Field()
-    while start < 10:
+    field = Field(1,20,400)
+    while start < 100:
+        print(f"Round: {start}")
         print(f"Grass:{field.grass}")
         print(f"Rabbit:{field.rabbit}")
         print(f"Wolf:{field.wolf}")
-        field.grass.live()
+        print(f"=============================")
+        field.step()
         start += 1
 
 if __name__ == "__main__":
